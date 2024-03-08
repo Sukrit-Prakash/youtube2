4ce160f0812a7e1b1d91aaf3fdc89383f74d467c
web_dev <sukritprakash2020@gmail,com>
2024-03-08 23:49:25 +0530

    user controllercommit

--- src/utils/asyncHanddler.js
+++ src/utils/asyncHanddler.js
@@ -1,6 +1,6 @@
-const asyncHandler =() =>{
+const asyncHandler =(requestHandler) =>{
     return (req,res,next)=>{
-        Promise.resolve(requsetHandler(req,res,next)).catch((err) => next(err))
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
     }
 }
 
